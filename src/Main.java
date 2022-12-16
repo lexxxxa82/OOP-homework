913e12b07031d48e15bcc07328866a690c54ff41
lexxxxa82 <lexxxxa.82@mail.ru>
2022-12-17 00:22:45 +0800

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human("Максим", "Минск", 1988,"бренд-менеджером");
+        Human maksim = new Human("Максим", "Минск", -1988,"бренд-менеджером");
         Human anya = new Human("Аня", "Москва", 1993,"методистом образовательных программ");
         Human Katia = new Human("Катя", " Калининград", 1992,"продакт-менеджером");
         Human Artem = new Human("Артем", "Москва", 1995,"директором по развитию бизнеса");
